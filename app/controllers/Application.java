4c69f0988de5d5b400acb3969dde821cc6cd8f2e
Victor Mendiluce <vmendi@gmail.com>
2014-04-02 11:29:54 -0700

    Heroku

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
-
+import play.mvc.Http.RequestBody;
 import views.html.*;
+
+class TestClass {
+	public String var1 = "Hello";
+	public String var2 = "Bye";
+}
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    public static Result test() {
+        //return ok(index.render("Your new application is ready."));
+    	return ok(Json.toJson(new TestClass()));
+    }
+    
+    public static Result ping() {
+    	// A little test of reading the application.conf
+    	return ok(ping.render(Play.application().configuration().getString("db.default.url")));
+    	//return ok("Pong " + dbUrl);
     }
 
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result jsontest() {
+    	RequestBody body = request().body();
+    	return ok("Got json: " + body.asJson());
+    }
 }
