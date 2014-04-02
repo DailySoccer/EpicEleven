7efd8375cdc26e206b16fd3f0a86cb88bedb42d7
Victor Mendiluce <vmendi@gmail.com>
2014-04-02 02:43:08 -0700

    Let there be light

---
+++ app/model/User.java
@@ -0,0 +1,21 @@
+package model;
+
+import javax.persistence.*;
+
+import play.db.ebean.*;
+
+@Entity
+public class User extends Model {
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String password;
+	
+	public User(String firstName, String lastName, String email, String password)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+}
