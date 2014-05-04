4b467c2b3c7da03272cc4fc14cb2f47cdfa4c823
Victor Mendiluce <vmendi@gmail.com>
2014-05-04 03:45:07 -0700

    We always commit before a new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
-git commit -am "Before deploy"
+git commit -am "We always commit before a new deploy"
 git checkout -b deploy
 rm public
 cd ../webclient
 ./build_rsync.sh
 cd ../backend
+git add .
 git commit -am "Including build in deploy branch"
 git push heroku deploy
+rm -rf public/
 git checkout master --force
 gir branch -D deploy
