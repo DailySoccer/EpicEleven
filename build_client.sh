b2e8475e843f5347b8393512f92406e32f788def
Victor Mendiluce <vmendi@gmail.com>
2014-05-04 03:41:13 -0700

    Before deploy

--- build_client.sh
+++ build_client.sh
@@ -1,11 +1,6 @@
-git commit -am "Before deploy"
-git checkout -b deploy
 rm public
 cd ../webclient
 ./build_rsync.sh
 cd ../backend
-git commit -am "Including build in deploy branch"
-git push heroku deploy
-git checkout master
-rem rm -rf public/
-rem ln -s ../webclient/build/web/ public
+rm -rf public/
+ln -s ../webclient/build/web/ public
