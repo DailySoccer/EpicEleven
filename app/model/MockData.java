710ff8e486449601d602b7d3b2c7130e823c435b
Victor Mendiluce <vmendi@gmail.com>
2014-05-15 22:26:18 -0700

    Remove old code for contest creation

--- app/model/MockData.java
+++ app/model/MockData.java
@@ -90,72 +90,18 @@ public final class MockData {
         return prefabMatchEvent;
     }
 
-    static private void createContests() {
+    static private void instantiateContests() {
 
-        DateTime currentCreationDay =  new DateTime(2014, 10, 14, 12, 0, DateTimeZone.UTC);
-
-        for (int dayCounter = 0; dayCounter < 10; ++dayCounter) {
-            for (int contestCounter = 0; contestCounter < 10; ++contestCounter) {
-
-                Contest contest = new Contest();
-
-                contest.matchEventIds = new ArrayList<>();
-                contest.currentUserIds = new ArrayList<>();
-                contest.maxUsers = 10;
-                contest.prizeType = PrizeType.STANDARD;
-                contest.entryFee = 10;
-                contest.salaryCap = 1000000;
-
-                contest.name = "Contest " + contestCounter + " date " + currentCreationDay;
-
-                for (int teamCounter = 0; teamCounter < 12; teamCounter += 2) {
-                    MatchEvent newMatch = createMatchEvent(String.format("Team%02d", teamCounter),
-                                                           String.format("Team%02d", teamCounter + 1),
-                                                           currentCreationDay);
-                    Model.matchEvents().insert(newMatch);
-                    contest.matchEventIds.add(newMatch.matchEventId);
-                }
-
-                Model.contests().insert(contest);
-            }
-
-            currentCreationDay.plusDays(3);
-        }
     }
 
-    static private MatchEvent createMatchEvent(final String teamA, final String teamB, final DateTime dateTime) {
+    static private MatchEvent instantiateMatchEvent() {
 
-        MatchEvent matchEvent = new MatchEvent();
-        matchEvent.startDate = dateTime.toDate();
-        matchEvent.soccerTeamA = createTeam(teamA);
-        matchEvent.soccerTeamB = createTeam(teamB);
-
-        return matchEvent;
+        return null;
     }
 
-    static private SoccerTeam createTeam(final String teamName) {
-        return new SoccerTeam() {{
-            name = teamName;
-            soccerPlayers = new ArrayList<SoccerPlayer>() {{
-                add(createSoccerPlayer(teamName, 0, FieldPos.GOALKEEPER));
+    static private SoccerTeam instantiateSoccerTeam() {
 
-                add(createSoccerPlayer(teamName, 1, FieldPos.DEFENSE));
-                add(createSoccerPlayer(teamName, 2, FieldPos.DEFENSE));
 
-                add(createSoccerPlayer(teamName, 3, FieldPos.MIDDLE));
-                add(createSoccerPlayer(teamName, 4, FieldPos.MIDDLE));
-
-                add(createSoccerPlayer(teamName, 5, FieldPos.FORWARD));
-                add(createSoccerPlayer(teamName, 6, FieldPos.FORWARD));
-            }};
-        }};
-    }
-
-    static private SoccerPlayer createSoccerPlayer(final String teamName, final int index, final FieldPos pos) {
-       return new SoccerPlayer() {{
-           name = String.format("SoccerPlayer%02d ", index) + pos.toString() + " " + teamName;
-           fieldPos = pos;
-           salary = 1000000;
-       }};
+        return null;
     }
 }
