a98eb3da59e75fc5791c0b0064128c34a4cc71e4
Ximo <xmalbors@hotmail.com>
2014-05-20 08:45:31 +0200

    ScalaTest

--- build.sbt
+++ build.sbt
@@ -7,10 +7,12 @@ libraryDependencies ++= Seq(
   filters,
   "org.mongodb" % "mongo-java-driver" % "2.12.0",    // https://github.com/mongodb/mongo-java-driver
   "org.jongo" % "jongo" % "1.0",                     // http://jongo.org/
-  // "org.apache.httpcomponents" % "httpclient" % "4.3.1",
-  "org.seleniumhq.selenium" % "selenium-java" % "2.41.0" % "test"
-  // "org.scalatest" % "scalatest_2.11" % "2.1.4" % "test"
+  "org.seleniumhq.selenium" % "selenium-java" % "2.41.0" % "test",
+  "org.scalatestplus" % "play_2.10" % "1.0.0" % "test",
+  "com.saucelabs" % "sauce_junit" % "2.0.5" % "test"
   // "org.mongojack" % "mongojack" % "2.0.0"
 )
+
+resolvers += "saucelabs-repository" at "http://repository-saucelabs.forge.cloudbees.com/release"
 
 play.Project.playJavaSettings
--- test/LoginTest.scala
+++ test/LoginTest.scala
@@ -1,63 +1,48 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
-
-import play.api.test._
-import play.api.test.Helpers._
-
-import org.fluentlenium.core.filter.FilterConstructor._
-import java.util.concurrent.TimeUnit
-import play.Logger
-
 import pages._
+import org.scalatestplus.play._
+import org.scalatest._
+import org.scalatest.{GivenWhenThen, ShouldMatchers}
+import org.openqa.selenium._
+import org.openqa.selenium.firefox._
+import org.openqa.selenium.chrome._
+import org.openqa.selenium.remote.{CapabilityType, DesiredCapabilities, RemoteWebDriver}
 
-class LoginTest extends PlaySpecification {
+class LoginTest extends SharedTest {
 
-  "Application" should {
+  before {
+  }
 
-    "load successfully" in new WithBrowser (webDriver = FIREFOX, port = 3333) {
-      val homePage = new HomePage(browser.getDriver, port, browser.getBaseUrl)
-      homePage.go
-      homePage.isAt
+  "User" must {
+    "go to home" in {
+      goToHomePage
     }
 
-    "select sign up in navigation menu" in new WithBrowser (webDriver = FIREFOX, port = 3333) {
-      val homePage = new HomePage(browser.getDriver, port, browser.getBaseUrl)
-      homePage.go
-      homePage.clickOnSignUp
-
-      browser.await().atMost(10, TimeUnit.SECONDS).untilPage().isLoaded()
-      browser.url must equalTo(SignUpPage.URL)
+    "home to sign up" in {
+      goToHomePage.clickOnSignUp
     }
 
-    "select login in navigation menu" in new WithBrowser (webDriver = FIREFOX, port = 3333) {
-      val homePage = new HomePage(browser.getDriver, port, browser.getBaseUrl)
-      homePage.go
-      homePage.clickOnLogin
-
-      browser.await().atMost(10, TimeUnit.SECONDS).untilPage().isLoaded()
-      browser.url must equalTo(LoginPage.URL)
+    "home to login" in {
+      goToHomePage.clickOnLogin
     }
 
-    "sign up user" in new WithBrowser (webDriver = FIREFOX, port = 3333) {
-      val signUpPage = new SignUpPage(browser.getDriver, port, browser.getBaseUrl)
-      signUpPage.go
-      signUpPage.isAt
-
-      browser.await().atMost(10, TimeUnit.SECONDS).untilPage().isLoaded()
-      signUpPage.fillAndSubmitForm("firstName", "lastName", "test@test.com", "nick", "private")
-      // browser.takeScreenShot
+    "signup" in {
+      goToSignUpPage.fillAndSubmitForm(Map(
+        "firstName" -> "First",
+        "lastName" -> "Last",
+        "email" -> "test@test.com",
+        "nick" -> "nick",
+        "password" -> "private"
+      ))
     }
 
-    "login user" in new WithBrowser (webDriver = FIREFOX, port = 3333) {
-      val loginPage = new LoginPage(browser.getDriver, port, browser.getBaseUrl)
-      loginPage.go
-      loginPage.isAt
-
-      browser.await().atMost(10, TimeUnit.SECONDS).untilPage().isLoaded()
-      loginPage.fillAndSubmitForm("test@test.com", "private")
-      // browser.takeScreenShot
+    "login" in {
+      goToLoginPage.fillAndSubmitForm(Map(
+        "email" -> "test@test.com",
+        "password" -> "private"
+      ))
     }
+  }
 
+  after {
   }
 }
