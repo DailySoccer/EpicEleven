9e995e86c23ddcc3e28480ef69c9a5830e49c047
Victor Mendiluce <vmendi@gmail.com>
2014-06-09 22:54:35 -0700

    Ahora el deploy no crea un commit sino que usa git stash

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-git commit -am "New deploy"
+# Cambiamos a la rama de deploy, asegurandonos de que esta recien creada
+git stash
 git branch -D deploy
 git checkout -b deploy
+
+# Tenemos que borrar el symlink y hacer una copia dura de toda la build
 rm public
 cd ../webclient
 ./build_rsync.sh
 cd ../backend
+
+# Añadimos nuevos archivos (la build) y commitimos
 git add .
 git commit -am "Including build in deploy branch"
+
+# Push a heroku
 git push heroku deploy:master --force
+
+# Vuelta a la rama principal
 git checkout master
 git branch -D deploy
+git stash apply
+
+# Restauramos el symlink borrado
+git checkout public
