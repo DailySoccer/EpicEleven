f531ea225f7ef5167db51029122dc10b2e4f0035
Fede Mon <gnu.fede@gmail.com>
2014-06-23 13:01:58 +0200

    more refactored models _ids to proper names

--- common/app/model/PointsTranslation.java
+++ common/app/model/PointsTranslation.java
@@ -7,8 +7,8 @@ import java.util.Date;
  * Created by gnufede on 02/06/14.
  */
 public class PointsTranslation {
-    public ObjectId _id;
-    public int eventTypeId; //eventTypeId
+    public ObjectId pointsTranslationId;
+    public int eventTypeId;
     public int points;
     public Date timestamp;
     public long unixtimestamp;
