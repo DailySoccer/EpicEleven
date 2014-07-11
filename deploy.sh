95b532f8b8df2de8bbdff7a1741d2104d229ec43
Fede Mon <gnu.fede@gmail.com>
2014-07-11 09:24:47 +0200

    initial changes

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,40 @@
 #!/bin/sh
 
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
+branch_name=${branch_name##refs/heads/}
+
 remotes_allowed_message="The only allowed Heroku remotes are: staging/production."
-
-if [ $# -eq 0 ] ; then
-    echo "You need to supply the Heroku remote. $remotes_allowed_message"
-    exit 1
-fi
-
-if [[ "$1" != "staging" && "$1" != "production" ]] ; then
-	echo $remotes_allowed_message
-    exit 1
-fi
 
 # Cambiamos a la rama de deploy, asegurandonos de que esta creada/reseateada
 git stash
+
+if [ $# -eq 0 ]
+    then
+        if [["$branch_name" != "master" && "$branch_name" != "develop" ]]
+            then
+                echo "You need to supply the Heroku remote. $remotes_allowed_message"
+                exit 1
+            else
+        elif [[ "$branch_name" == "master" ]]
+            1 = "production"
+        elif [[ "$branch_name" == "develop" ]]
+            1 = "staging"
+        fi
+    else
+        if [[ "$1" != "staging" && "$1" != "production" ]]
+            then
+                echo $remotes_allowed_message
+                exit 1
+        elif [[ "$1" == "staging" ]]
+            git checkout develop
+        elif [[ "$1" != "production" ]]
+            git checkout master
+        fi
+fi
+
+
+
 git checkout -B deploy
 
 # Tenemos que borrar el symlink y hacer una copia dura de toda la build
