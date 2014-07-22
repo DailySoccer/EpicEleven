c160d18ce7da8410641f3625a12f24e2b170d797
Fede Mon <gnu.fede@gmail.com>
2014-07-22 16:43:19 +0200

    Deletes previous snapshot if exists

--- data/snapshot_dump.sh
+++ data/snapshot_dump.sh
@@ -4,9 +4,8 @@ mypwd=$PWD
 rand=/tmp/snapshot-$1-$RANDOM
 
 if [[ ! -f $name && ! -d $name && ! -L $name ]];
-    then
-        mkdir $rand && mongodump -d snapshot -o $rand && cd $rand && tar czf $mypwd/$name snapshot
-        cd $mypwd && rm -rf $rand
-    else 
-        echo "el fichero $name existe, borralo primero"
+then
+    rm $name
 fi
+mkdir $rand && mongodump -d snapshot -o $rand && cd $rand && tar czf $mypwd/$name snapshot
+cd $mypwd && rm -rf $rand
