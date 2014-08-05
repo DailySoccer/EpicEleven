53c3eae2e7300174afd4065d6758b63814189f47
Ximo <xmalbors@hotmail.com>
2014-08-05 14:56:28 +0200

    PrizeType.FREE

--- common/app/model/PrizeType.java
+++ common/app/model/PrizeType.java
@@ -5,10 +5,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public enum PrizeType {
-    WINNER_TAKES_ALL(0),
-    TOP_3_GET_PRIZES(1),
-    TOP_THIRD_GET_PRIZES(2),
-    FIFTY_FIFTY(3),
+    FREE(0),
+    WINNER_TAKES_ALL(1),
+    TOP_3_GET_PRIZES(2),
+    TOP_THIRD_GET_PRIZES(3),
+    FIFTY_FIFTY(4),
     STANDARD(-1);
 
     public final int id;
