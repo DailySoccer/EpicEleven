244b32cf1c815ab1930d7b08b5f08915201f6b09
Fede Mon <gnu.fede@gmail.com>
2014-08-21 11:40:41 +0200

    Added capability to deploy to staging from a different branch than develop

--- deploy.sh
+++ deploy.sh
@@ -22,13 +22,7 @@ fi
 
 if [ $# -eq 0 ]
     then
-        if [[ "$branch_name" != "master" && "$branch_name" != "develop" ]]
-            then
-                #echo "You need to supply the Heroku remote. $remotes_allowed_message"
-                echo "If you want to push $branch_name to staging, you must say explicitly:"
-                echo "> ./deploy staging"
-                exit 1
-        elif [[ "$branch_name" == "master" ]]
+        if [[ "$branch_name" == "master" ]]
             then
                 destination="production"
         else 
@@ -39,7 +33,7 @@ if [ $# -eq 0 ]
             then
                 echo $remotes_allowed_message
                 exit 1
-        elif [[ "$1" != "production" && "$branch_name" != "master" ]]
+        elif [[ "$1" != "staging" && "$1" != "production" && "$branch_name" != "master" ]]
             then
                 git checkout master
         fi
