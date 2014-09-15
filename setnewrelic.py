de75a3d2e83376bf5f5214a9590aba84ee92c8d7
Fede Mon <gnu.fede@gmail.com>
2014-09-15 16:44:20 +0200

    Functional version

--- setnewrelic.py
+++ setnewrelic.py
@@ -1,4 +1,6 @@
-#!/bin/env python
+#!/usr/bin/env python
+import sys
+import shlex
 import subprocess
 
 APP = sys.argv[1]
@@ -6,19 +8,24 @@ NEW_STATE = sys.argv[2]
 
 
 def get_setter_cmd(java_opts):
+    """
+    Construye el comando de 'set' con las nuevas JAVA_OPTS
+    """
     return 'heroku config:set JAVA_OPTS="{}" --app {}'.format(java_opts, APP)
 
 
 RELIC_OPTS = ' -J-javaagent:newrelic/newrelic.jar -J-Dnewrelic.config.file=newrelic/newrelic.yml'
 CONFIG_GET_CMD = 'heroku config:get JAVA_OPTS --app {}'.format(APP)
 
-heroku_opts = subprocess.call(CONFIG_GET_CMD)
+heroku_opts = subprocess.check_output(shlex.split(CONFIG_GET_CMD))[0:-1]
 
-NEW_OPTS = None
-if RELIC_OPTS in heroku_opts and NEW_STATE == 'off':
-    NEW_OPTS = heroku_opts[0:heroku_opts.find(RELIC_OPTS)]
-elif RELIC_OPTS not in heroku_opts and NEW_STATE == 'on':
-    NEW_OPTS = heroku_opts + RELIC_OPTS
+# Ponemos o quitamos las opciones de Relic si cambiamos de estado
+NEW_OPTS = heroku_opts + RELIC_OPTS                      if RELIC_OPTS not in heroku_opts and NEW_STATE == 'on' else \
+           heroku_opts[0:heroku_opts.find(RELIC_OPTS)]   if RELIC_OPTS in heroku_opts and NEW_STATE == 'off' else \
+           None
 
 if NEW_OPTS:
-    subprocess.call(get_setter_cmd(NEW_OPTS))
+    # Si cambiamos de estado ejecutamos el comando de heroku
+    heroku_opts = subprocess.check_output(shlex.split(get_setter_cmd(NEW_OPTS)))[0:-1]
+
+print heroku_opts
