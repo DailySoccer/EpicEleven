b7624258edba0f0d948c09358e2874619568e6fb
Fede Mon <gnu.fede@gmail.com>
2014-09-16 16:49:15 +0200

    Log posted message and level

--- app/controllers/LoggerController.java
+++ app/controllers/LoggerController.java
@@ -2,16 +2,24 @@ package controllers;
 
 import actions.AllowCors;
 import play.Logger;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import static play.data.Form.form;
+
 @AllowCors.Origin
 public class LoggerController extends Controller {
-
-    public static Result postLog() {
-        Logger.error("WTF 101: Client exception:");
+    public static class Params {
+        public String level;
+        public String errorMessage;
+    }
+    public static Result logPost() {
+        Form<Params> errorForm = form(Params.class).bindFromRequest();
+        Params params = errorForm.get();
+        Logger.error("WTF 101: Client exception: {}", params.level);
         Logger.error("--------------------------");
-        Logger.error(request().body().asText());
+        Logger.error(params.errorMessage);
         Logger.error("--------------------------");
         return ok("Stacktrace logged");
     }
