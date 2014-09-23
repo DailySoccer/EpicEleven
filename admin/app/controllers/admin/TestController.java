1f2c09972591a9f27524b83625e1cd70c3f357d4
Fede Mon <gnu.fede@gmail.com>
2014-09-23 17:43:00 +0200

    Test: Ir a fecha facilmente

--- admin/app/controllers/admin/TestController.java
+++ admin/app/controllers/admin/TestController.java
@@ -4,8 +4,11 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class TestController extends Controller {
+
     static public Result start() {
         if (!OptaSimulator.isCreated())
             OptaSimulator.init();
@@ -13,6 +16,23 @@ public class TestController extends Controller {
         OptaSimulator.instance().reset(false);
         return ok("OK");
     }
+
+
+
+    static public Result gotoDateTest(int year, int month, int day, int hour, int minute) {
+
+        if (!OptaSimulator.isCreated())
+            OptaSimulator.init();
+
+        GregorianCalendar myDate = new GregorianCalendar(year, month-1, day, hour, minute);
+
+        myDate.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        OptaSimulator.instance().gotoDate(myDate.getTime());
+
+        return ok("OK");
+    }
+
 
     static public Result gotoDate(Long timestamp) {
         Date date = new Date(timestamp);
@@ -32,5 +52,9 @@ public class TestController extends Controller {
     static public Result importEverything() {
         DashboardController.importEverything();
         return ok("OK");
+    }
+
+    static public Result getCurrentDate() {
+        return ok(SimulatorController.getCurrentDate());
     }
 }
