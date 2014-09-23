9482a483fc93cf445cd260fb68e6535e02b317d7
Fede Mon <gnu.fede@gmail.com>
2014-09-23 14:31:31 +0200

    Stormpath gives me headaches

--- common/app/model/stormpath/StormPathClient.java
+++ common/app/model/stormpath/StormPathClient.java
@@ -1,11 +1,15 @@
 package model.stormpath;
 
+import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.api.ApiKey;
 import com.stormpath.sdk.api.ApiKeys;
-import com.stormpath.sdk.application.ApplicationList;
+import com.stormpath.sdk.application.Application;
+import com.stormpath.sdk.application.Applications;
 import com.stormpath.sdk.client.Client;
 import com.stormpath.sdk.client.Clients;
+import com.stormpath.sdk.directory.CustomData;
 import com.stormpath.sdk.tenant.Tenant;
+
 
 
 public class StormPathClient {
@@ -18,10 +22,27 @@ public class StormPathClient {
 
     Tenant tenant = client.getCurrentTenant();
 
-    ApplicationList myApplications = client.getApplications();
 
 
+    com.stormpath.sdk.application.Application application = client.instantiate(Application.class);
+
+    application.setName("My Awesome Application"); //must be unique among your other apps
+    application = client.createApplication(
+            Applications.newCreateRequestFor(application).createDirectory().build());
 
 
+    //Create the account object
+    Account account = client.instantiate(Account.class);
 
+    //Set the account properties
+    account.setGivenName("Joe");
+    account.setSurname("Stormtrooper");
+    account.setUsername("tk421"); //optional, defaults to email if unset
+    account.setEmail("tk421@stormpath.com");
+    account.setPassword("Changeme1");
+    CustomData customData = account.getCustomData();
+    customData.put("favoriteColor", "white");
+
+//Create the account using the existing Application object
+    application.createAccount(account);
 }
