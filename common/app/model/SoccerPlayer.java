7c7746ba50eb9161946499b0cc6bd8c2d913beb5
Ximo <ximo.m.albors@gmail.com>
2014-09-24 14:29:45 +0200

    updated JsonViews.NotForClient.class

--- common/app/model/SoccerPlayer.java
+++ common/app/model/SoccerPlayer.java
@@ -1,14 +1,27 @@
 package model;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import org.bson.types.ObjectId;
 
 public class SoccerPlayer {
     public ObjectId templateSoccerPlayerId;
+
+    @JsonView(JsonViews.NotForClient.class)
     public String optaPlayerId;
+
+    @JsonView(JsonViews.Public.class)
     public String name;
+
+    @JsonView(JsonViews.Public.class)
     public FieldPos fieldPos;
+
+    @JsonView(JsonViews.Public.class)
     public int salary;
+
+    @JsonView(JsonViews.Public.class)
     public int playedMatches;
+
+    @JsonView(JsonViews.Public.class)
     public int fantasyPoints;
 
     public SoccerPlayer() { }
