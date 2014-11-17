e7c4c0dd1740b2e8495c2a81e5ec7b60fe604fae
Ximo <ximo.m.albors@gmail.com>
2014-11-17 15:02:48 +0100

    Product como enum

--- common/app/model/Product.java
+++ common/app/model/Product.java
@@ -1,29 +1,18 @@
 package model;
 
-import java.util.HashMap;
-import java.util.Map;
+public enum Product {
+    PRODUCT_1   ("Product 1: Platinum", 10),
+    PRODUCT_2   ("Product 2: Gold", 20);
 
-public class Product {
-    enum ProductType {
-        PRODUCT_1,
-        PRODUCT_2
-    }
-    static Map<String, Product> productsMap = new HashMap<String, Product>() {
-        {
-            put(ProductType.PRODUCT_1.toString(), new Product("Product 1: Platinum", 10));
-            put(ProductType.PRODUCT_2.toString(), new Product("Product 2: Gold", 20));
-        }
-    };
+    public final String name;
+    public final int price;
 
-    public String name;
-    public int price;
-
-    public Product (String name, int price) {
+    Product (String name, int price) {
         this.name = name;
         this.price = price;
     }
 
-    static public Product findOne(String productType) {
-        return productsMap.get(productType);
+    static public Product findOne(String productId) {
+        return Product.valueOf(productId);
     }
 }
