e2012a6364878f62848b1a20d7784d317b5e66f9
vmendi <vmendi@gmail.com>
2014-12-25 17:52:41 -0800

    Eliminamos build.sbt de los subprojectos

--- project/build.scala
+++ project/build.scala
@@ -4,11 +4,12 @@
 import play.PlayJava
 import sbt._
 import sbt.Keys._
+import play.Play.autoImport._
 
 import java.io.PrintWriter
 import scala.io.Source
 
-object BackendBuild extends Build {
+object build extends Build {
 
   lazy val commonSettings = Seq(
     version := "1.0.0",
@@ -51,6 +52,38 @@ object BackendBuild extends Build {
     val common = Project(id = "common",
                          base = file("./common"))
                 .settings(commonSettings:_*)
+                .settings(libraryDependencies ++= Seq(
+                     javaJdbc
+                    ,"org.mongodb" % "mongo-java-driver" % "2.12.3"
+                    ,"org.jongo" % "jongo" % "1.0"
+                    ,"org.jdom" % "jdom" % "2.0.2"
+                    ,"postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+                    ,"commons-dbutils" % "commons-dbutils" % "1.6"           // http://commons.apache.org/proper/commons-dbutils/index.html
+                    ,"org.jooq" % "jooq" % "3.5.0"
+                    ,"org.jooq" % "jooq-meta" % "3.5.0"
+                    ,"org.flywaydb" % "flyway-core" % "3.1"
+                  ))
+                // No queremos las carpetas por defecto de sbt (este no es un PlayProject), definimos las nuestras con un solo root.
+                // Por no ser un proyecto play, hay que definir a mano el encoding
+                .settings(sourceDirectory in Compile := baseDirectory.value / "app",
+                          scalaSource in Compile := baseDirectory.value / "app",
+                          javaSource in Compile := baseDirectory.value / "app",
+                          javacOptions := List("-encoding", "utf-8"))
+
+    var backend = Project(id = "backend",
+                          base = file("."))
+                  .enablePlugins(PlayJava)
+                  .settings(commonSettings:_*)
+                  .settings(libraryDependencies ++= Seq(
+                     cache
+                    ,filters
+                    ,javaJdbc
+                    ,javaWs
+                    ,"com.stormpath.sdk" % "stormpath-sdk-api" % "1.0.RC2"
+                    ,"com.stormpath.sdk" % "stormpath-sdk-httpclient" % "1.0.RC2"
+                  ))
+                 .aggregate(common)
+                 .dependsOn(common)
 
     if (file("./admin").exists()) {
       val admin = Project(id = "admin",
@@ -58,29 +91,28 @@ object BackendBuild extends Build {
                  .dependsOn(common)
                  .enablePlugins(PlayJava)
                  .settings(commonSettings:_*)
+                 .settings(libraryDependencies ++= Seq(
+                    cache
+                   ,filters
+                   ,javaWs
+                   ,"com.google.gdata" % "core" % "1.47.1"
+                   ,"org.apache.oltu.oauth2" % "org.apache.oltu.oauth2.client" % "1.0.0"
+                 ))
 
-      val backend = Project(id = "backend",
-                            base = file("."))
-                    .aggregate(common, admin)
-                    .dependsOn(common, admin)
-                    .enablePlugins(PlayJava)
-                    .settings(commonSettings:_*)
+      backend = backend.aggregate(admin)
+                       .dependsOn(admin)
 
       Seq(common, admin, backend)
     }
     else {
-      val backend = Project(id = "backend",
-                            base = file("."))
-                    .aggregate(common)
-                    .dependsOn(common)
-                    .enablePlugins(PlayJava)
-                    .settings(removeAdminFromRouterTask,
-                              compile in Compile <<= (compile in Compile).dependsOn(removeAdminFromRouter))
-                    .settings(commonSettings:_*)
+      backend = backend.settings(removeAdminFromRouterTask,
+                                 compile in Compile <<= (compile in Compile).dependsOn(removeAdminFromRouter))
 
       Seq(common, backend)
     }
   }
+
+  // javacOptions := List("-Xlint:unchecked")
 
   // Hacemos el hook de las rutas hijas (por ejemplo, admin/) dentro del fichero de routas ('backend.routes'), como
   // esta documentado que hay que hacerlo, con la sintaxis de flecha "-> /admin admin.routes". Esto genera un warning
