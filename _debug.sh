e424f3917e7142c9340ed19fb875c47134bef641
vmendi <vmendi@gmail.com>
2015-01-14 15:35:42 -0800

    El default debe ser http

--- _debug.sh
+++ _debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Run your application using ~run to enable direct compilation on file change.
-activator -jvm-debug 9999 "run -Dhttp.port=disabled -Dhttps.port=9000 -Dhttps.keyStore=conf/keystore.jks -Dhttps.keyStorePassword=asdfasdf"
+activator -jvm-debug 9999 run
--- _debug_http.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Run your application using ~run to enable direct compilation on file change.
-activator -jvm-debug 9999 run
