7085c229a3fc6373e1ec6b81cc5d1abeac894930
Ximo <ximo.m.albors@gmail.com>
2015-02-06 08:41:12 +0100

    refactor JacksonJodaMoney

--- common/app/model/JacksonJodaMoney.java
+++ common/app/model/JacksonJodaMoney.java
@@ -1,35 +1,23 @@
 package model;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
 import org.joda.money.Money;
 
 import java.io.IOException;
 
 public class JacksonJodaMoney {
-    public static class MoneySerializer extends StdSerializer<Money> {
-        protected MoneySerializer() {
-            super(Money.class);
-        }
-
+    public static class MoneySerializer extends JsonSerializer<Money> {
         @Override
-        public void serialize(Money value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
-            jgen.writeString(value.toString());
+        public void serialize(Money value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(value.toString());
         }
     }
 
-    public static class MoneyDeserializer extends StdDeserializer<Money> {
-        protected MoneyDeserializer() {
-            super(Money.class);
-        }
-
+    public static class MoneyDeserializer extends JsonDeserializer<Money> {
         @Override
-        public Money deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
-            return Money.parse(jp.readValueAs(String.class));
+        public Money deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            return Money.parse(jsonParser.readValueAs(String.class));
         }
     }
 }
