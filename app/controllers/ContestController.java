ffd1e02bb9a3d272741f29947091a32850270b3b
Ximo <ximo.m.albors@gmail.com>
2015-11-19 13:09:20 +0100

    fix ContestController: createContest: prizeMultiplier

--- app/controllers/ContestController.java
+++ app/controllers/ContestController.java
@@ -104,6 +104,8 @@ public class ContestController extends Controller {
 
                 // Logger.debug("createContest: {} - {}", params.templateContestId, params.name);
 
+                final float SIMULATION_PRIZE_MULTIPLIER = 10f;
+
                 // Por defecto, los contests creados por los usuarios esperarán a que el author entre un contestEntry
                 contest.state = ContestState.WAITING_AUTHOR;
                 contest.authorId = theUser.userId;
@@ -112,6 +114,7 @@ public class ContestController extends Controller {
                 contest.maxEntries = params.maxEntries;
                 contest.freeSlots = params.maxEntries;
                 contest.entryFee = params.simulation ? Money.zero(MoneyUtils.CURRENCY_ENERGY).plus(templateContest.entryFee.getAmount()) : templateContest.entryFee;
+                contest.prizeMultiplier = params.simulation ? SIMULATION_PRIZE_MULTIPLIER : contest.prizeMultiplier;
 
                 Model.contests().update("{_id: #}", contest.contestId).upsert().with(contest);
 
