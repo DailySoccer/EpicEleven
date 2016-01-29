736fed786a729bb23cfe6be5658d75c7d74399f3
Ximo <ximo.m.albors@gmail.com>
2016-01-29 13:27:52 +0100

    fix TrueSkill: ScheduleLoop: MAX_ITERATIONS

--- common/app/jskills/factorgraphs/ScheduleLoop.java
+++ common/app/jskills/factorgraphs/ScheduleLoop.java
@@ -1,8 +1,10 @@
 package jskills.factorgraphs;
 
+import play.Logger;
+
 public class ScheduleLoop<T> extends Schedule<T> {
 
-    private static final int MAX_ITERATIONS = 100;
+    private static final int MAX_ITERATIONS = 1000;
 
     private final double maxDelta;
     private final Schedule<T> scheduleToLoop;
@@ -16,13 +18,25 @@ public class ScheduleLoop<T> extends Schedule<T> {
     @Override
     public double visit(int depth, int maxDepth) {
         double delta = scheduleToLoop.visit(depth + 1, maxDepth);
-        for(int totalIterations = 1; delta > maxDelta; totalIterations++) {
+
+        int totalIterations = 1;
+        for(totalIterations = 1; delta > maxDelta; totalIterations++) {
             delta = scheduleToLoop.visit(depth + 1, maxDepth);
-            if(totalIterations > MAX_ITERATIONS)
+
+            if(totalIterations > MAX_ITERATIONS) {
+                /*
                 throw new RuntimeException(String.format(
                         "Maximum iterations (%d) reached.", MAX_ITERATIONS));
+                */
+                break;
+            }
         }
-        
+
+        if(totalIterations > 100) {
+            Logger.warn("ScheduleLoop: iterations: {} delta: {}", totalIterations, delta);
+        }
+        // Logger.warn("ScheduleLoop: iterations: {} delta: {}", totalIterations, delta);
+
         return delta;
     }
 }
