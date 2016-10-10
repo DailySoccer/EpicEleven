50b5f609b03053747d7edf87443fd6df6e9b5a0a
korgan00 <korgan00@hotmail.com>
2016-10-10 17:30:54 +0200

    f

--- app/controllers/StoreController.java
+++ app/controllers/StoreController.java
@@ -176,7 +176,6 @@ public class StoreController extends Controller {
                 new F.Function<WSResponse, JsonNode>() {
                     public JsonNode apply(WSResponse response) {
                         try {
-                            Logger.debug("Json respuesta as text: {}", response.asJson().asText() );
                             Logger.debug("Json respuesta as json: {}", response.asJson() );
                             return response.asJson();
                         }
