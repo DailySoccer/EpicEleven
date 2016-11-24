0a589133620f4572aa9b8b648783d045f8f38ac8
Ximo <ximo.m.albors@gmail.com>
2016-11-24 09:38:14 +0100

    PromoController: No hacer ninguna query para buscar Promos

--- app/controllers/PromoController.java
+++ app/controllers/PromoController.java
@@ -4,12 +4,17 @@ import actions.AllowCors;
 import actions.UserAuthenticated;
 import com.google.common.collect.ImmutableMap;
 import model.Promo;
+import play.cache.Cached;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.With;
 import utils.ReturnHelper;
+
+import java.util.ArrayList;
 
 @AllowCors.Origin
 public class PromoController extends Controller {
+    private final static int CACHE_PROMO = 12 * 60 * 60;            // 12 horas
 
     private static final String ERROR_VIEW_PROMO_INVALID = "ERROR_VIEW_PROMO_INVALID";
     private static final String ERROR_MY_PROMO_INVALID = "ERROR_MY_PROMO_INVALID";
@@ -21,8 +26,11 @@ public class PromoController extends Controller {
         return new ReturnHelper(ImmutableMap.of("promos", Promo.findByCodeName(codeName))).toResult();
     }
 
+    @With(AllowCors.CorsAction.class)
+    @Cached(key = "Promos", duration = CACHE_PROMO)
     public static Result getPromos() {
-        return new ReturnHelper(ImmutableMap.of("promos", Promo.getCurrent())).toResult();
+        // TODO No hay promos en esta versión
+        return new ReturnHelper(ImmutableMap.of("promos", new ArrayList<Promo>() /*Promo.getCurrent()*/)).toResult();
     }
 
 }
