f7e68fd6e52d4e497109be43ac2bd285ad7ff047
Ximo <ximo.m.albors@gmail.com>
2016-12-16 07:55:38 +0100

    build: jongo "1.3.0" / mongo-java-driver "3.4.0"

--- project/build.scala
+++ project/build.scala
@@ -62,8 +62,8 @@ object build extends Build {
                 .settings(libraryDependencies ++= Seq(
                     javaJdbc
                     ,javaWs
-                    ,"org.mongodb" % "mongo-java-driver" % "2.13.3"
-                    ,"org.jongo" % "jongo" % "1.1"
+                    ,"org.mongodb" % "mongo-java-driver" % "3.4.0"
+                    ,"org.jongo" % "jongo" % "1.3.0"
                     ,"org.jdom" % "jdom" % "2.0.2"
                     ,"org.joda" % "joda-money" % "0.10.0"
                     ,"postgresql" % "postgresql" % "9.1-901-1.jdbc4"
